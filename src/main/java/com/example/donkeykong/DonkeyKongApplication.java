9e5e5a94db684829218f659d8f5206edf5e45632
AdamPach <adam.pach@outlook.cz>
2023-10-21 11:22:45 +0200

    Improve structure for the DonkeyKongApplication.java class

--- src/main/java/com/example/donkeykong/DonkeyKongApplication.java
+++ src/main/java/com/example/donkeykong/DonkeyKongApplication.java
@@ -5,24 +5,36 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.stage.WindowEvent;
 
 public class DonkeyKongApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
 
-        Group root = new Group();
-        Canvas canvas = new Canvas(800, 600);
-        root.getChildren().add(canvas);
-        Scene scene = new Scene(root);
+        try
+        {
+            Group root = new Group();
+            Canvas canvas = new Canvas(800, 600);
+            root.getChildren().add(canvas);
+            Scene scene = new Scene(root);
 
-        stage.setTitle("DonkeyKong🙈");
-        stage.setScene(scene);
-        stage.show();
+            stage.setTitle("DonkeyKong🙈");
+            stage.setScene(scene);
+            stage.show();
+            stage.setOnCloseRequest(this::OnCloseRequest);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    private void OnCloseRequest(WindowEvent event)
+    {
+        System.exit(0);
     }
 }
