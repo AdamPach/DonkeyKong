0a7ed144632a2d1efe6c9f0f4d4daea83a96792b
AdamPach <adam.pach@outlook.cz>
2023-10-25 08:42:38 +0200

    Complete demo level design

--- src/main/java/com/adampach/donkeykong/world/Level.java
+++ src/main/java/com/adampach/donkeykong/world/Level.java
@@ -23,6 +23,8 @@ public class Level implements Drawable, Simulable {
         textures = new ArrayList<>();
         textures.add(new LevelBorders(levelSettings));
         textures.add(new Ladder(550, 425, 25, 100));
+        textures.add(new Ladder(25, 265, 25, 100));
+        textures.add(new Ladder(550, 115, 25, 100));
         textures.add(new Construction(0, 575, 100, 25));
         textures.add(new Construction(100, 565, 100, 25));
         textures.add(new Construction(200, 555, 100, 25));
@@ -30,6 +32,23 @@ public class Level implements Drawable, Simulable {
         textures.add(new Construction(400, 535, 100, 25));
         textures.add(new Construction(500, 525, 100, 25));
         textures.add(new Construction(500, 425, 100, 25));
+        textures.add(new Construction(400, 415, 100, 25));
+        textures.add(new Construction(300, 395, 100, 25));
+        textures.add(new Construction(200, 385, 100, 25));
+        textures.add(new Construction(100, 375, 100, 25));
+        textures.add(new Construction(0, 365, 100, 25));
+        textures.add(new Construction(0, 265, 100, 25));
+        textures.add(new Construction(100, 255, 100, 25));
+        textures.add(new Construction(200, 245, 100, 25));
+        textures.add(new Construction(300, 235, 100, 25));
+        textures.add(new Construction(400, 225, 100, 25));
+        textures.add(new Construction(500, 215, 100, 25));
+        textures.add(new Construction(500, 115, 100, 25));
+        textures.add(new Construction(400, 115, 100, 25));
+        textures.add(new Construction(300, 115, 100, 25));
+        textures.add(new Construction(200, 115, 100, 25));
+        textures.add(new Construction(100, 115, 100, 25));
+        textures.add(new Construction(0, 115, 100, 25));
     }
 
 
